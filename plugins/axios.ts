e134c5bcf4a0fb6487ec2dd4efa812ab20b8c621
masaki takahashi <bje35149@gmail.com>
2023-04-18 20:36:30 +0900

    サンプルコミット

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -8,6 +8,6 @@ export default function ({ $axios, redirect }: Context) {
   $axios.onError((error) => {
     const code = error.response?.status;
 
-    if (code === 404) console.log("404 Error");
+    if (code === 404) console.log("Code:404 Error");
   });
 }
