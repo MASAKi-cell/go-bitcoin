718dee1c8a9656b4684a5da0b2ec5d26437cd823
masaki takahashi <bje35149@gmail.com.q!exit>
2023-04-18 20:34:51 +0900

    axiosの設定追加

---
+++ plugins/axios.ts
@@ -0,0 +1,13 @@
+import { Context } from "@nuxt/types";
+
+export default function ({ $axios, redirect }: Context) {
+  $axios.onRequest((config) => {
+    console.log(`API url:${config.url}, method:${config.method}`);
+  });
+
+  $axios.onError((error) => {
+    const code = error.response?.status;
+
+    if (code === 404) console.log("404 Error");
+  });
+}
