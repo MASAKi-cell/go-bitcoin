470fb628055779eb859f7f413e3fbc0239197443
masaki takahashi <bje35149@gmail.com>
2023-04-21 20:58:22 +0900

    eslintの設定

--- .eslint-rules.js
+++ .eslint-rules.js
@@ -1,1 +1,31 @@
-module.exports = {}
+/**
+ * eslint:recommended
+ * https://eslint.org/docs/rules/
+ */
+
+module.exports = {
+
+  // 等式演算子 === 、 !== の使用 ==、!=の使用禁止
+  eqeqeq: ['error', 'always'],
+
+  // オブジェクト(={})の先頭と末尾にはスペースをあける
+  'object-curly-spacing': ['error', 'always'],
+
+  // ifやtryの前後にスペースを設定する
+  'keyword-spacing': [
+    'error',
+    {
+      before: true,
+      after: true,
+    },
+  ],
+
+  // 三項演算子の改行を統一する
+  'multiline-ternary': ['error', 'always-multiline'],
+
+  // ◯シングルクオートの設定
+  quotes: ['error', 'single'],
+
+  // インデントはスペース2つ。
+  indent: ['error', 2, { SwitchCase: 1 }],
+}
